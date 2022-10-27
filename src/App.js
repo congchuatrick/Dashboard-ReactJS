a8ad165f7e1c7d627aa6e6105c4517db77762741
congchuatrick <teamtricker140502001@gmail.com>
2022-10-27 13:38:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports';
 import Products from './pages/Products';
+import { FooterContainer } from './containers/footer'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
+      <FooterContainer />
     </>
   );
 }
